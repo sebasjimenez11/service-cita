d5e3c4f092f2eca4382c16a43821305d42387e28
sebasJimenez1121 <jimenez1121monroy@gmail.com>
2024-09-20 04:19:27 -0500

    actualiacion url para el cron

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 10102;
-const HOSTCROON = process.env.HOST_CROON || "http://localhost:10102/medicamentos/getAlarms";
+const HOSTCROON = process.env.HOST_CROON || "service-cita-production.up.railway.app/medicamentos/getAlarms";
 
 import citas from './router/citaRouter';
 import pagos from './router/pagoRouter';
